b999dae0cb32ffdea19bcc9942029e32c0b9b284
Niklas Schreiber <niklas@derschreiber.com>
2014-12-11 15:48:57 +0100

    get api uri from dom instead of hard coded

--- app/assets/javascripts/store/spree_elastic_search.js
+++ app/assets/javascripts/store/spree_elastic_search.js
@@ -12,42 +12,41 @@ if (Spree.routes == undefined) {
 if (Spree.api == undefined) {
     Spree.api = {}
 }
-// TODO Set this from config / env vars!!
-Spree.routes.product_search = "http://meinwohnstore.dev/api/products";
-var engine = new Bloodhound({
-    limit: 10,
-    datumTokenizer: function (d) {
-        return Bloodhound.tokenizers.whitespace(d.value);
-    },
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    remote: {
-        url: Spree.routes.product_search + '?token=%KEY&q=%QUERY&taxon=%TAXON&autocomplete=true&template=index_autocomplete',
-        replace: function(url, query){
-            return url.replace("%QUERY", query)
-                        .replace("%TAXON", $("#search-select").val())
-                        .replace("%KEY", Spree.api_key || "f993ba069ad462543f3d3f7741d5628c4557ade74644a592");
-        },
-        filter: function (data) {
-            return $.map(data.products, function (product) {
-                return {
-                    //total_count: data.total_count,
-                    value: product.name,
-                    brand: product.manufacturer_name,
-                    category: product.category_name,
-                    link: product.permalink
-                };
-            });
-        }
-    }
-});
-
-var promise = engine.initialize();
-promise
-    .done(function() { })
-    .fail(function() { });
-
 
 $(function() {
+    var engine = new Bloodhound({
+        limit: 10,
+        datumTokenizer: function (d) {
+            return Bloodhound.tokenizers.whitespace(d.value);
+        },
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: {
+            url: Spree.routes.product_search + '?token=%KEY&q=%QUERY&taxon=%TAXON&autocomplete=true&template=index_autocomplete',
+            replace: function(url, query){
+                return url.replace("%QUERY", query)
+                            .replace("%TAXON", $("#search-select").val())
+                            .replace("%KEY", Spree.api_key || "f993ba069ad462543f3d3f7741d5628c4557ade74644a592");
+            },
+            filter: function (data) {
+                return $.map(data.products, function (product) {
+                    return {
+                        //total_count: data.total_count,
+                        value: product.name,
+                        brand: product.manufacturer_name,
+                        category: product.category_name,
+                        link: product.permalink
+                    };
+                });
+            }
+        }
+    });
+
+    var promise = engine.initialize();
+    promise
+        .done(function() { })
+        .fail(function() { });
+
+
     $('.typeahead').typeahead({
         hint: true,
         highlight: true,
