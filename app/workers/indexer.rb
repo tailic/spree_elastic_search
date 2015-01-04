344748606aef5c50150ec4d27cf18fc01a020fe2
Niklas Schreiber <niklas@derschreiber.com>
2015-01-04 18:22:01 +0100

    get right environment configs from yml

--- app/workers/indexer.rb
+++ app/workers/indexer.rb
@@ -16,7 +16,7 @@ class Indexer
   }
 
   if File.exists?('config/elasticsearch.yml')
-    config.merge!(YAML.load_file('config/elasticsearch.yml').symbolize_keys)
+    config.merge!(YAML.load_file('config/elasticsearch.yml')[Rails.env].symbolize_keys)
   end
 
   Elasticsearch::Model.client = Elasticsearch::Client.new(config)
