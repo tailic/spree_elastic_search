9d5739f454528b180aa3ec63c534c08a6ea53c3a
Niklas Schreiber <niklas@derschreiber.com>
2015-05-27 18:13:07 +0200

    remove async callbacks

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -75,10 +75,10 @@ module Searchable
 
     # Set up callbacks for updating the index on model changes
     #
-    after_commit lambda { Indexer.perform_async(:index, self.class.to_s, self.id) }, on: :create
-    after_commit lambda { Indexer.perform_async(:update, self.class.to_s, self.id) }, on: :update
-    after_commit lambda { Indexer.perform_async(:delete, self.class.to_s, self.id) }, on: :destroy
-    after_touch lambda { Indexer.perform_async(:update, self.class.to_s, self.id) }
+    #after_commit lambda { Indexer.perform_async(:index, self.class.to_s, self.id) }, on: :create
+    #after_commit lambda { Indexer.perform_async(:update, self.class.to_s, self.id) }, on: :update
+    #after_commit lambda { Indexer.perform_async(:delete, self.class.to_s, self.id) }, on: :destroy
+    #after_touch lambda { Indexer.perform_async(:update, self.class.to_s, self.id) }
 
     #def import
     #  delegate :import, to: :__elasticsearch__ if self.is_a? Variant
