7fbd435e7d6e74605e4f6f1bcc54a361076e6649
Niklas Schreiber <niklas@derschreiber.com>
2015-11-03 12:01:10 +0100

    cleanup

--- app/assets/javascripts/spree/frontend/spree_elastic_search.js
+++ app/assets/javascripts/spree/frontend/spree_elastic_search.js
@@ -9,12 +9,3 @@ if (Spree.routes == undefined) {
 if (Spree.api == undefined) {
     Spree.api = {}
 }
-
-$(function() {
-    $("#sort").change(function(data) {
-        if(data.target.value){
-            uri = URI(document.url).setSearch("sort", data.target.value);
-            document.location = uri
-        }
-    })
-});
